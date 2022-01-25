374913138879f55cd31a3f46e9db4b57b407c42f
Maksim <galuza.maksim996@gmail.com>
2022-01-25 13:29:49 +0600

    add cards

---
+++ app.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll( '.slide' )
+
+function clearActiveClasses() {
+    slides.forEach( slide => slide.classList.remove( 'active' ) )
+}
+
+slides.forEach( slide => {
+    slide.addEventListener( 'click', () => {
+        clearActiveClasses()
+
+        slide.classList.add( 'active' )
+    })
+})
+
+
